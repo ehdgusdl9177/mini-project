eb241b9d72587c913c8e7e51f5a257deeb72d944
ehdgusdl9177 <k23129177@gmail.com>
2023-12-04 17:55:50 +0900

    change title winner

--- Dicee Project/index.js
+++ Dicee Project/index.js
@@ -15,3 +15,11 @@ let image2 = document.querySelectorAll("img")[1];
 let randomImageSource2 = `images/dice${randomNumber2}.png`;
 
 image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
