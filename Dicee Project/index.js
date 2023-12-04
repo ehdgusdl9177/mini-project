03d9fc8f9d8abe2c13a6bcd2e9bf643f3867ccad
ehdgusdl9177 <k23129177@gmail.com>
2023-12-04 17:49:45 +0900

    change the img

--- Dicee Project/index.js
+++ Dicee Project/index.js
@@ -1,3 +1,17 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImage = `dice${randomNumber1}.png`;
+let randomDiceImage1 = `dice${randomNumber1}.png`;
+
+let randomImageSource1 = `images/${randomDiceImage1}`;
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let image2 = document.querySelectorAll("img")[1];
+
+let randomImageSource2 = `images/dice${randomNumber2}.png`;
+
+image2.setAttribute("src", randomImageSource2);
