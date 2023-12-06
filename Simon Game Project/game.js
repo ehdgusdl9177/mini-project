e0ef1a51894eea1bf3a5f449bbc9bec7cedfe53c
ehdgusdl9177 <k23129177@gmail.com>
2023-12-06 16:34:57 +0900

    show sequence and check button press

--- Simon Game Project/game.js
+++ Simon Game Project/game.js
@@ -1,8 +1,22 @@
 let buttonColurs = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColurs[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+  audio.play();
 }
