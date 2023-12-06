46033e00f1cc400784df3ea20e2e5d9de7464e61
ehdgusdl9177 <k23129177@gmail.com>
2023-12-06 11:41:17 +0900

    jquery element

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,20 @@
-$("h1").css("color", "red");
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+$("input").keypress(function (event) {
+  console.log(event.key);
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "blue");
+});
+
+//$("h1").before("<button>New</button>");
+
+//$("h1").after("<button>ROA</button>");
+
+$("h1").prepend("<button>New</button>");
+
+$("h1").append("<button>ROA</button>");
