d17b0189360091049425fdac26d31462f61e8939
ehdgusdl9177 <k23129177@gmail.com>
2024-01-26 11:54:10 +0900

    User pages

--- Side Project/frontend/src/user/pages/Users.js
+++ Side Project/frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "keivn",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FtEMUl%2FbtrDc6957nj%2FNwJoDw0EOapJNDSNRNZK8K%2Fimg.jpg",
+      plcaes: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
