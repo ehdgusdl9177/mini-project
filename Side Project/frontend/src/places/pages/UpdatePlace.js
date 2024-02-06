fa373e2be05051f325152589dab8b1e14779c2bd
ehdgusdl9177 <k23129177@gmail.com>
2024-02-06 11:31:32 +0900

    UpdatePlace component

---
+++ Side Project/frontend/src/places/pages/UpdatePlace.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.kkday.com/ko/blog/wp-content/uploads/%EB%89%B4%EC%9A%95-3%EB%8C%80-%EC%A0%84%EB%A7%9D%EB%8C%80-%EC%97%A0%ED%8C%8C%EC%9D%B4%EC%96%B4-%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%8A%B8-%EB%B9%8C%EB%94%A9-%EC%99%B8%EA%B4%80.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.kkday.com/ko/blog/wp-content/uploads/%EB%89%B4%EC%9A%95-3%EB%8C%80-%EC%A0%84%EB%A7%9D%EB%8C%80-%EC%97%A0%ED%8C%8C%EC%9D%B4%EC%96%B4-%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%8A%B8-%EB%B9%8C%EB%94%A9-%EC%99%B8%EA%B4%80.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedplace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedplace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>;
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={() => {}}
+        value={identifiedplace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)"
+        onInput={() => {}}
+        value={identifiedplace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
