b33fc6e796d4584dc299a133910e1303b6442c07
ehdgusdl9177 <k23129177@gmail.com>
2024-04-26 16:39:09 +0900

    add place controllers

--- Side Project/backend/controllers/places-controllers.js
+++ Side Project/backend/controllers/places-controllers.js
@@ -1,3 +1,17 @@
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((p) => {
@@ -10,3 +24,22 @@ const getPlaceById = (req, res, next) => {
 
   res.json({ place });
 };
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
