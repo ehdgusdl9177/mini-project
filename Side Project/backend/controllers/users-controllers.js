3f0be592d06a18fca85e20dd4f66e4d721a8a698
ehdgusdl9177 <k23129177@gmail.com>
2024-05-03 18:09:58 +0900

    create getUser route

--- Side Project/backend/controllers/users-controllers.js
+++ Side Project/backend/controllers/users-controllers.js
@@ -1,4 +1,18 @@
-const getUsers = (req, res, next) => {};
+const { v4: uuidv4 } = require("uuid");
+const uuid = uuidv4();
+
+const DUMMY_PLACES = [
+  {
+    id: "u1",
+    name: "Max Schwarz",
+    email: "test@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_PLACES });
+};
 
 const signup = (req, res, next) => {};
 
