5f95ee0491a00557174247681acadceb1c4403de
ehdgusdl9177 <k23129177@gmail.com>
2024-05-07 20:39:32 +0900

    add validation places

--- Side Project/backend/controllers/places-controllers.js
+++ Side Project/backend/controllers/places-controllers.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
 const uuid = uuidv4();
 
+const { validationResult } = require("express-validator");
+
 const HttpError = require("../models/http-error");
 
 let DUMMY_PLACES = [
@@ -51,6 +53,12 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   // const title = req.body.title;
   const createdPlace = {
