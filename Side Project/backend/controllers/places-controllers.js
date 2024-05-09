908b00828ca4921fe009f8fc7dcf1455634a6ebe
ehdgusdl9177 <k23129177@gmail.com>
2024-05-09 21:13:33 +0900

    update patch route

--- Side Project/backend/controllers/places-controllers.js
+++ Side Project/backend/controllers/places-controllers.js
@@ -76,6 +76,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlaceById = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
