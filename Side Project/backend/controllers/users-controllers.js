47eda51e9cb530b1f602b1c3df8b22ad159c2fe7
ehdgusdl9177 <k23129177@gmail.com>
2024-05-09 21:17:53 +0900

    update user validator

--- Side Project/backend/controllers/users-controllers.js
+++ Side Project/backend/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const uuid = uuidv4();
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -17,6 +18,11 @@ const getUsers = (req, res, next) => {
 };
 
 const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_PLACES.find((u) => u.email === email);
