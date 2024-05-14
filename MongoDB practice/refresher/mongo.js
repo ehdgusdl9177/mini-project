ebe14ae0ab48115289671ab81f98827f52e69a1b
ehdgusdl9177 <k23129177@gmail.com>
2024-05-14 20:10:24 +0900

    connect mongoDB

---
+++ MongoDB practice/refresher/mongo.js
@@ -0,0 +1,10 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = `mongodb+srv://hyeon9811:QLRYc2RzPhPvOLiO@cluster0.a4t4m8y.mongodb.net/places_test?retryWrites=true&w=majority&appName=Cluster0`;
+
+const createProduct = async (req, res, next) => {};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
