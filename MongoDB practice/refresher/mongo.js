157f293c20add0cbd76a3df81386d0d78a6abbb1
ehdgusdl9177 <k23129177@gmail.com>
2024-05-16 17:21:26 +0900

    connect mongoDB

--- MongoDB practice/refresher/mongo.js
+++ MongoDB practice/refresher/mongo.js
@@ -2,7 +2,23 @@ const MongoClient = require("mongodb").MongoClient;
 
 const url = `mongodb+srv://hyeon9811:QLRYc2RzPhPvOLiO@cluster0.a4t4m8y.mongodb.net/places_test?retryWrites=true&w=majority&appName=Cluster0`;
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  }
+  client.close();
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
