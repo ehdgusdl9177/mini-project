d69904849a8a6747ec15b8572838d54a4e11395b
ehdgusdl9177 <k23129177@gmail.com>
2024-05-16 17:29:15 +0900

    connect mongoDB

--- MongoDB practice/refresher/mongo.js
+++ MongoDB practice/refresher/mongo.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 
-const url = `mongodb+srv://hyeon9811:QLRYc2RzPhPvOLiO@cluster0.a4t4m8y.mongodb.net/places_test?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://hyeon9811:QLRYc2RzPhPvOLiO@cluster0.a4t4m8y.mongodb.net/products_test?retryWrites=true&w=majority&appName=Cluster0`;
 
 const createProduct = async (req, res, next) => {
   const newProduct = {
