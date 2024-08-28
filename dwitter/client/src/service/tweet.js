79baa38510ea827e38fe51a873dedbf130936eb1
ehdgusdl9177 <k23129177@gmail.com>
2024-08-28 15:00:44 +0900

    repectoring client

--- dwitter/client/src/service/tweet.js
+++ dwitter/client/src/service/tweet.js
@@ -1,53 +1,31 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
   async getTweets(username) {
     const query = username ? `?username=${username}` : "";
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
+    return this.http.fetch(`/tweets${query}`, {
       mathod: "GET",
-      headers: { "Content-Type": "application/json" },
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    return this.http.fetch(`/tweets`, {
       mathod: "POST",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text, username: "kevin", name: "Kevin" }),
     });
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       mathod: "DELETE",
-      headers: { "Content-Type": "application/json" },
     });
-    if (response.status !== 204) {
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       mathod: "PUT",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
