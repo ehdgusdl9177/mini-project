1837937c5fcedf2cef5a78093641ff11413e279c
ehdgusdl9177 <k23129177@gmail.com>
2024-08-28 15:04:26 +0900

    repectoring conponents

--- dwitter/client/src/components/EditTweetForm.jsx
+++ dwitter/client/src/components/EditTweetForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const EditTweetForm = ({ tweet, onUpdate, onClose }) => {
   const [text, setText] = useState(tweet.text);
@@ -14,21 +14,21 @@ const EditTweetForm = ({ tweet, onUpdate, onClose }) => {
   };
 
   return (
-    <form className='edit-tweet-form' onSubmit={onSubmit}>
+    <form className="edit-tweet-form" onSubmit={onSubmit}>
       <input
-        type='text'
-        placeholder='Edit your tweet'
+        type="text"
+        placeholder="Edit your tweet"
         value={text}
         required
         autoFocus
         onChange={onChange}
-        className='form-input tweet-input'
+        className="form-input tweet-input"
       />
-      <div className='edit-tweet-form-action'>
-        <button type='submit' className='form-btn-update'>
+      <div className="edit-tweet-form-action">
+        <button type="submit" className="form-btn-update">
           Update
         </button>
-        <button type='button' className='form-btn-cancel' onClick={onClose}>
+        <button type="button" className="form-btn-cancel" onClick={onClose}>
           Cancel
         </button>
       </div>
